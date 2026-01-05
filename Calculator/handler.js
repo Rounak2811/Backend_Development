ef4fe3ec4153675c53bd8ffe30f94bfed13f5aba
ROUNAK KUMAR <kumarrounakjha@gmail.com>
2026-01-05 23:49:29 +0530

    making a calculator project

---
+++ Calculator/handler.js
@@ -0,0 +1,53 @@
+const requestHandler=(req,res)=>{
+    if(req.url==='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write(
+        `<html>
+            <head>
+                <title>Calculator Project</title>
+            </head>
+            <body>
+                <h1><center>Welcome To the Calculator Project</center></h1>
+                <h2><a href="/calculator">Go To Calculator</a></h2>
+            </body>
+        </html>`
+        );
+        return res.end();
+    }
+    else if(req.url==='/calculator'){
+        res.setHeader('Content-Type','text/html');
+        res.write(
+        `<html>
+            <head>
+                <title>Calculator Screen</title>
+            </head>
+            <body>
+                <form action="/calculate-result" method="POST">
+                    <label for="first">First Number:</label>
+                    <input type="number" value="first" id="first">
+                    <br><br>
+                    <label for="second">Second Number:</label>
+                    <input type="number" value="second" id="second">
+                    <br><br>
+                    <input type="submit" value="Sum">
+                </form>
+            </body>
+            </html>`
+        );
+        return res.end();
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write(
+        `<html>
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Hello guys</h1>
+        </body>
+        </html>`
+    );
+    res.end();
+}
+
+module.exports=requestHandler;
