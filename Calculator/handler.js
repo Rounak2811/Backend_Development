879b5cbded45298544b26e56345835147b964853
ROUNAK KUMAR <kumarrounakjha@gmail.com>
2026-01-06 13:55:09 +0530

    added add modules to modularity

--- Calculator/handler.js
+++ Calculator/handler.js
@@ -1,3 +1,6 @@
+
+const sumRequestHandler=require('./sum');
+
 const requestHandler=(req,res)=>{
     if(req.url==='/'){
         res.setHeader('Content-Type','text/html');
@@ -37,31 +40,7 @@ const requestHandler=(req,res)=>{
         return res.end();
     }
     else if(req.url==='/calculate-result' && req.method==='POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            res.setHeader('Content-Type', 'text/html');
-            const fullBody = Buffer.concat(body).toString();
-            const params = new URLSearchParams(fullBody);
-            const first = Number(params.get('first')) || 0;
-            const second = Number(params.get('second')) || 0;
-            const sum = first + second;
-            res.write(`
-                <html>
-                    <head><title>Result</title></head>
-                    <body>
-                        <h1>Result</h1>
-                        <p>First: ${first}</p>
-                        <p>Second: ${second}</p>
-                        <h2>Sum: ${sum}</h2>
-                        <a href="/calculator">Back</a>
-                    </body>
-                </html>
-            `);
-            return res.end();
-        });
+        sumRequestHandler(req,res);
     }
 }
 
